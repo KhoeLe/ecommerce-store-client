9686d230479425043966c0f6c1b95a3e6b2bde1a
louis.le@kmail.vn <khoe.le.mall@gmail.com>
2023-07-24 15:43:22 +0700

    #feat: define the images format

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const nextConfig = {
         serverActions: true,
     },
     images: {
-        domains: ["res.cloudinary.com", "louisle.s3.ap-southeast-1.amazonaws.com"],
+        domains: [
+            "res.cloudinary.com",
+            "louisle.s3.ap-southeast-1.amazonaws.com",
+        ],
         deviceSizes: [320, 640, 768, 1024, 1600], // Define the device sizes for responsive images
         imageSizes: [16, 32, 48, 64, 96], // Define the image sizes to be generated
+        formats: ["image/webp"], // Define the image formats to be generated
     },
 };
 
